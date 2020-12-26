5f5b8e68ead4d5b4514cb803aaebaa6cee9bae91
Chris Backhouse <cjbackhouse@gmail.com>
2020-12-26 23:43:40 +0000

    Rework with saner inheritance hierarchy

--- autodiff.cxx
+++ autodiff.cxx
@@ -9,112 +9,131 @@ template<int X> std::string var_name()
   return std::to_string(X);
 }
 
-template<int X> class OneDiff
+template<int X> struct OneDiff
 {
-public:
-  OneDiff() {}
-
-  OneDiff(double d) : diff(d) {}
-
-  void Print(std::ostream& os) const
-  {
-    os << "d/d" << var_name<X>() << " = " << diff << std::endl;
-  }
-
   double diff;
 };
 
-template<int... Xs> class Diffs;
 
-// Constant value, no derivs
-template<> class Diffs<>
+template<int... Xs> class Diffs : protected OneDiff<Xs>...
 {
 public:
-  Diffs() {}
-
-  Diffs(double v) : val(v) {}
-
-  template<int... Xs> Diffs<Xs...> operator+(const Diffs<Xs...>& d) const
+  void Print(std::ostream& os) const
   {
-    //    std::cout << __PRETTY_FUNCTION__ << std::endl;
-    Diffs<Xs...> ret = d;
-    ret.val += val;
-    return ret;
+    os << "Value = " << val << std::endl;
+    Print<Xs...>(os);
   }
 
-  void Print(std::ostream& os) const {os << "Value = " << val << std::endl;}
+  template<int i> const double& diff() const{return OneDiff<i>::diff;}
+  template<int i>       double& diff()      {return OneDiff<i>::diff;}
+
   double val;
+
+ protected:
+  template<int Y, int... Ys> void Print(std::ostream& os) const
+  {
+    os << "  d/d" << var_name<Y>() << " = " << diff<Y>() << std::endl;
+    if constexpr(sizeof...(Ys) > 0) Print<Ys...>(os);
+  }
 };
 
-template<int i, int... Xs> double get_diff(const Diffs<Xs...>& d)
+template<int... Xs> struct Indices;
+
+template<> struct Indices<>
 {
-  return ((OneDiff<i>*)&d)->diff;
+  static const bool empty = true;
+  static const int head = std::numeric_limits<int>::max(); // TODO bad hack
+  static const Indices<> tail;
+
+  typedef Diffs<> Diffs_t;
+};
+
+template<int X, int... Xs> struct Indices<X, Xs...>
+{
+  static const int head = X;
+  static const Indices<Xs...> tail;
+  static const bool empty = false;
+
+  typedef Diffs<X, Xs...> Diffs_t;
+};
+
+
+template<int... Xs, int... Ys> Indices<Xs..., Ys...>
+Concat(const Indices<Xs...>& a, const Indices<Ys...>& b)
+{
+  return Indices<Xs..., Ys...>();
 }
 
-template<int i, int... Xs> double set_diff(const Diffs<Xs...>& d, double val)
+template<int... Xs, int... Ys> auto Zip(const Indices<Xs...>& a,
+                                        const Indices<Ys...>& b)
 {
-  ((OneDiff<i>*)&d)->diff = val;
+  if constexpr(a.empty) return b;
+  if constexpr(b.empty) return a;
+
+  if constexpr(!a.empty && !b.empty){
+    if constexpr(a.head < b.head ) return Concat(Indices<a.head>(), Zip(a.tail, b));
+    if constexpr(a.head == b.head) return Concat(Indices<a.head>(), Zip(a.tail, b.tail));
+    if constexpr(a.head > b.head ) return Concat(Indices<b.head>(), Zip(a, b.tail));
+  }
+
+  abort();
 }
 
-template<int... Ys> void CopyDiffs(const Diffs<>& from, Diffs<Ys...>& to)
+template<int... Xs, int... Ys> void CopyDiffs(Indices<> is, const Diffs<Xs...>& from, Diffs<Ys...>& to)
 {
 }
 
-template<int X, int... Xs, int... Ys> void CopyDiffs(const Diffs<X, Xs...>& from, Diffs<Ys...>& to)
+template<int... Zs, int... Xs, int... Ys> void CopyDiffs(Indices<Zs...> is, const Diffs<Xs...>& from, Diffs<Ys...>& to)
 {
-  set_diff<X>(to, get_diff<X>(from));
-  CopyDiffs(Diffs<Xs...>(from), to);
+  to.template diff<is.head>() = from.template diff<is.head>();
+  /*if constexpr(sizeof...(Zs) > 0)*/ CopyDiffs(is.tail, from, to);
 }
 
 // Add d/dX = d to b (X < Xs[0]) TODO figure out static_assert
 template<int X, int... Xs> Diffs<X, Xs...> Concat(double d,
                                                   const Diffs<Xs...>& b)
 {
-  //  std::cout << __PRETTY_FUNCTION__ << std::endl;
-
   Diffs<X, Xs...> ret;
   ret.val = b.val;
-  CopyDiffs(b, ret);
-  set_diff<X>(ret, d);
+  CopyDiffs(Indices<Xs...>(), b, ret);
+  ret.template diff<X>() = d;
   return ret;
 }
 
-template<int X, int... Xs> class Diffs<X, Xs...> : protected OneDiff<X>, protected Diffs<Xs...>
+template<int... Xs, int... Ys, int... Ws, int... Zs>
+typename decltype(Zip(Indices<Xs...>(), Indices<Ys...>()))::Diffs_t
+//auto
+Add(Indices<Xs...> i, Indices<Ys...> j,
+    const Diffs<Ws...>& a, const Diffs<Zs...>& b)
 {
-public:
-  // Friend with all other variants of self
-  template<int... Ys> friend class Diffs;
-  template<int Y, int... Ys> friend Diffs<Y, Ys...> Concat(double, const Diffs<Ys...>&);
-  template<int Y, int... Ys, int... Zs> friend void CopyDiffs(const Diffs<Y, Ys...>&, Diffs<Zs...>& to);
+  if constexpr(i.empty && j.empty){
+    Diffs<> ret;
+    ret.val = a.val + b.val;
+    return ret;
+  }
+  else{
+    if constexpr(j.empty || i.head < j.head){
+      return Concat<i.head>(a.template diff<i.head>(), Add(i.tail, j, a, b));
+    }
 
-  Diffs() {}
+    if constexpr(i.head == j.head){
+      return Concat<i.head>(a.template diff<i.head>() + b.template diff<i.head>(),
+                            Add(i.tail, j.tail, a, b));
+    }
 
-  template<class... Args> Diffs(double v, double d, Args... ds) : OneDiff<X>(d), Diffs<Xs...>(v, ds...) {}
-
-  void Print(std::ostream& os) const
-  {
-    OneDiff<X>::Print(os);
-    Diffs<Xs...>::Print(os);
+    if constexpr(i.empty || j.head < i.head){
+      return Concat<j.head>(b.template diff<j.head>(), Add(i, j.tail, a, b));
+    }
   }
 
-  template<int Y, int... Ys> auto operator+(const Diffs<Y, Ys...>& d) const
-  {
-    //    std::cout << __PRETTY_FUNCTION__ << std::endl;
+  abort();
+}
 
-    if constexpr(X < Y){
-      return Concat<X>(get_diff<X>(*this),
-                       Diffs<Xs...>(*this) + d);
-    }
-    else if constexpr(X == Y){
-      return Concat<X>(get_diff<X>(*this) + get_diff<X>(d),
-                       Diffs<Xs...>(*this) + Diffs<Ys...>(d));
-    }
-    else{ // X > Y
-      return Concat<Y>(get_diff<Y>(d),
-                       *this + Diffs<Ys...>(d));
-    }
-  }
-};
+template<int... Xs, int... Ys> auto
+operator+(const Diffs<Xs...>& a, const Diffs<Ys...>& b)
+{
+  return Add(Indices<Xs...>(), Indices<Ys...>(), a, b);
+}
 
 template<int... Xs> std::ostream& operator<<(std::ostream& os, const Diffs<Xs...>& d)
 {
@@ -122,20 +141,30 @@ template<int... Xs> std::ostream& operator<<(std::ostream& os, const Diffs<Xs...
   return os;
 }
 
-// Specialize
+// Specialize names
 template<> std::string var_name<1>(){return "x";}
 template<> std::string var_name<2>(){return "y";}
 template<> std::string var_name<3>(){return "z";}
 
 int main()
 {
-  Diffs<1> d(7., 2.);
+  Diffs<1> d;
+  d.val = 7;
+  d.diff<1>() = 2;
 
-  Diffs<1, 2, 3> e(8., 3., 4., 5.);
+  Diffs<1, 2, 3> e;
+  e.val = 8;
+  e.diff<1>() = 3;
+  e.diff<2>() = 4;
+  e.diff<3>() = 5;
 
-  Diffs<2, 3> f(1., 6., 7.);
+  Diffs<2, 3> f;
+  f.val = 1;
+  f.diff<2>() = 6;
+  f.diff<3>() = 7;
 
   std::cout << "d:\n" << d << std::endl;
+
   std::cout << "e:\n" << e << std::endl;
   std::cout << "f:\n" << f << std::endl;
 
@@ -145,8 +174,6 @@ int main()
   std::cout << "d+f:\n" << (d+f) << std::endl;
 
   std::cout << "e+f:\n" << (e+f) << std::endl;
-
-  //  std::cout << "d*e:\n" << (d*e) << std::endl;
 
   return 0;
 }
